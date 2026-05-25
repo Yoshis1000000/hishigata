885967fd268892b3a807b41ad575b031e9d53848
Yoshis1000000 <163902924+Yoshis1000000@users.noreply.github.com>
2026-05-25 12:05:15 +0100

    Fixed Over-Rotation Bug
    Prevented notes from continuing to rotate after reaching their intended destination via the use of an AbsoluteSequence.

--- osu.Game.Rulesets.Hishigata/Mods/HishigataModApproachDifferent.cs
+++ osu.Game.Rulesets.Hishigata/Mods/HishigataModApproachDifferent.cs
@@ -3,6 +3,7 @@ using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Transforms;
 using osu.Framework.Localisation;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Drawables;
@@ -29,19 +30,22 @@ namespace osu.Game.Rulesets.Hishigata.Mods
 
         public void ApplyToDrawableHitObject(DrawableHitObject drawableHitObject)
         {
-            drawableHitObject.ApplyCustomUpdateState += (drawableHitObject, _) =>
+            drawableHitObject.ApplyCustomUpdateState += (DrawableHitObject drawableHitObject, ArmedState _) =>
             {
                 if (drawableHitObject is DrawableHishigataNote drawableHishigataNote)
                 {
                     HishigataNote hishigataNote = drawableHishigataNote.HitObject;
-                    if (hishigataNote.IsFeign)
+                    using (drawableHishigataNote.BeginAbsoluteSequence(hishigataNote.StartTime - hishigataNote.TimePreempt))
                     {
-                        drawableHishigataNote.ClearTransforms();
-                        drawableHishigataNote.RotateTo(0).FadeColour(Color4Extensions.FromHex("ff0064")).Then().RotateTo(90, hishigataNote.TimePreempt * .5).Delay(hishigataNote.TimePreempt * .5).Then().RotateTo(270, 200).FadeColour(Color4.White, 200).Delay(200).Then().RotateTo(360, hishigataNote.TimePreempt * .5);
-                    }
-                    else
-                    {
-                        drawableHishigataNote.RotateTo(180).Then().RotateTo(360, hishigataNote.TimePreempt);
+                        if (hishigataNote.IsFeign)
+                        {
+                            drawableHishigataNote.ClearTransforms(targetMember: nameof(drawableHishigataNote.Rotation));
+                            //drawableHishigataNote.RotateTo(0).FadeColour(Color4Extensions.FromHex("ff0064")).Then().RotateTo(90, hishigataNote.TimePreempt * .5).Delay(hishigataNote.TimePreempt * .5).Then().RotateTo(270, 200).FadeColour(Color4.White, 200).Delay(200).Then().RotateTo(360, hishigataNote.TimePreempt * .5);
+                        }
+                        else
+                        {
+                            drawableHishigataNote.RotateTo(180).Then().RotateTo(360, hishigataNote.TimePreempt);
+                        }
                     }
                 }
             };
